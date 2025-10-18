1b00a73c7646d812e9ccafff102eeb068f3af089
masonchenus <masonchenus@gmail.com>
2025-10-18 17:46:38 -0400

    Adjust trajectories array size from 3 to 5
    Increased the size of trajectories arrays for better data handling.

--- js/trajectories.js
+++ js/trajectories.js
@@ -7,8 +7,8 @@
  * @return {object} Trajectory state object
  */
 export function init(positions, size=10000) {
-  let trajectories1 = Array(size * 3).fill(0);
-  let trajectories2 = Array(size * 3).fill(0);
+  let trajectories1 = Array(size * 5).fill(0);
+  let trajectories2 = Array(size * 5).fill(0);
 
   // Store positions of the two galaxy cores
   // ------
